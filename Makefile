be7ee24e27a54982204abcbd0e82af27c905ff5a
joaoafonso07 <joaoafonsomestre@gmail.com>
2022-12-23 16:39:37 +0000

    Reading Input
    Reading Input implemented and tested
    Makefile implemented and tested

---
+++ Makefile
@@ -0,0 +1,23 @@
+# Makefile
+#
+# It uses implicit rules to compile .c files using the
+# values assigned to macros CC and CFLAGS
+#
+
+#the compiler: g++ for C++ 
+CC = g++
+
+#compiler flags
+CFLAGS = -std=c++11 -O3 -Wall
+
+#The build target
+TARGET = project
+
+all: $(TARGET)
+
+$(TARGET): $(TARGET).cpp
+			$(CC) $(CFLAGS) -o gerador $(TARGET).cpp -lm
+
+
+clean:
+	$(RM) $(TARGET)
--- project.cpp
+++ project.cpp
@@ -8,28 +8,38 @@
 
 
 int main() {
-    short V, E, u, w, v; //V = number of vertices, E = number of eges
+    int V, E, u, w, v; //V = number of vertices, E = number of eges
     unsigned long long result;
     bool empty = true;
-    if(scanf("%hu", &V) == -1)
+    if(scanf("%i", &V) == -1)
         return -1;
-    if(scanf("%hu", &E) == -1)
+    if(scanf("%i", &E) == -1)
         return -1;
 
-    std::vector<std::vector<short> > c; //vector with e number of vectors representing a heavy edge (u, w, v) 
+    std::vector<std::vector<int> > c(E, std::vector<int>(3)); //vector with e number of vectors representing a heavy edge (u, w, v) 
 
     /*
     Reads e number of heavy eges (u, w, v) being u and v two vertices conected with a w weight
     */
     for(short i = 0; i < E; i++){ // for each i less than E add the vector to the vector c
-        if(scanf("%hu %hu %hu", &u, &w, &v) != 1)
+        if(scanf("%i %i %i", &u, &w, &v) != 3){
+            printf("something wronf with scanf\n");//debug
             return -1;
+        }
+
+        printf("u: %i\n", u);//debug
         c[i].push_back(u);
+
+        printf("w: %i\n", w);//debug
         c[i].push_back(w);
+
+        printf("v: %i\n", v);//debug
         c[i].push_back(v);
+
         if(empty == true && c[i][1] != 0)
             empty = false;
     }
+
     if(!empty){
         result = 1; //function to develop
     }else
