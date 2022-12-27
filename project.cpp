5476446b1db0a971cd18d65839bfa57321afd562
joaoafonso07 <joaoafonsomestre@gmail.com>
2022-12-27 16:50:15 +0000

    Project probably completed
    -not necessary includes deleted
    -some bugs fixed
    -main algorithm routine implemented
    -project is functional and tested
    
    Remains:
    Organize code;
    Add more commnets;

--- project.cpp
+++ project.cpp
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 #include <vector>
-#include <unordered_map>
 
 typedef struct {
     int parent;
@@ -21,13 +19,9 @@ void make_set(size_t i){
 }
 
 int find_set(int x){
-    if (tree[x-1].parent != x);
+    if (tree[x-1].parent != x)
         tree[x-1].parent = find_set(tree[x-1].parent);
     return tree[x-1].parent;
-}
-
-void union(int x, int y){
-    link(find_set[x], find_set(y));
 }
 
 void link(int x, int y){
@@ -41,10 +35,13 @@ void link(int x, int y){
     }
 }
 
+void union_find(int x, int y){
+    link(find_set(x), find_set(y));
+}
 
 int main() {
     int E, u, w, v; //V = number of vertices, E = number of eges
-    unsigned long long result;
+    unsigned long long result = 0;
     bool empty = true;
 
     if(scanf("%lu", &tree_size) == -1)
@@ -59,13 +56,14 @@ int main() {
         return -1;
 
     std::vector<std::vector<int>> edges;  //vector with e number of vectors representing a heavy edge (u, v, w) 
+    
 
     /*
     Reads e number of heavy eges (u, v, w) being u and v two vertices conected with a w weight
     */
     for(short i = 0; i < E; i++){ // for each i less than E add the vector to the vector c
         if(scanf("%i %i %i", &u, &v, &w) != 3){
-            printf("something wronf with scanf\n");//debug
+            printf("something wrong with scanf\n");//debug
             return -1;
         }
         edges.push_back({u, v, w});
@@ -87,20 +85,23 @@ int main() {
         return a[2] > b[2];
     });
 
-    printf("\n +++ \n");
+    //printf("\n +++ \n"); //debug
 
     
     for (auto i : edges){
-        printf("%i %i %i\n", i[0], i[1], i[2]);
-
+        //printf("%i %i %i\n", i[0], i[1], i[2]); //debug
+        if(find_set(i[0]) != find_set(i[1])){
+            union_find(i[0], i[1]);
+            result += i[2];
+        }
     }
 
-
+    /*
     if(!empty){
         result = 1; //function to develop
     }else
         result = 0;
-
+    */
     std::cout << result << "\n";
     return 0;
 }
