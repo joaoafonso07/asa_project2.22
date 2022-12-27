71f433096d149e4a7f756f9db4a4850256e0b758
joaoafonso07 <joaoafonsomestre@gmail.com>
2022-12-27 13:48:28 +0000

    Debug, Edges sort and make set
    There was a bug building the vector - fixed.
    Vector sorting implemneted and tested.
    Make set implemented.

--- project.cpp
+++ project.cpp
@@ -6,39 +6,80 @@
 #include <vector>
 #include <unordered_map>
 
+typedef struct {
+    int parent;
+    int rank;
+} node;
+
+static node *tree;
+size_t tree_size;
+
+
+void make_set(size_t i){
+    tree[i].rank = 0;
+    tree[i].parent = i + 1;
+}
+
 
 int main() {
-    int V, E, u, w, v; //V = number of vertices, E = number of eges
+    int E, u, w, v; //V = number of vertices, E = number of eges
     unsigned long long result;
     bool empty = true;
-    if(scanf("%i", &V) == -1)
+
+    if(scanf("%lu", &tree_size) == -1)
         return -1;
+
+    tree = (node*)malloc(sizeof(node)*tree_size);
+    if(tree == NULL){
+        return -1;
+    }
+
     if(scanf("%i", &E) == -1)
         return -1;
 
-    std::vector<std::vector<int> > c(E, std::vector<int>(3)); //vector with e number of vectors representing a heavy edge (u, w, v) 
+    std::vector<std::vector<int> > edges; //(E, std::vector<int>(3)); //vector with e number of vectors representing a heavy edge (u, v, w) 
 
     /*
-    Reads e number of heavy eges (u, w, v) being u and v two vertices conected with a w weight
+    Reads e number of heavy eges (u, v, w) being u and v two vertices conected with a w weight
     */
     for(short i = 0; i < E; i++){ // for each i less than E add the vector to the vector c
-        if(scanf("%i %i %i", &u, &w, &v) != 3){
+        if(scanf("%i %i %i", &u, &v, &w) != 3){
             printf("something wronf with scanf\n");//debug
             return -1;
         }
-
+        edges.push_back({u, v, w});
+        /*
         printf("u: %i\n", u);//debug
-        c[i].push_back(u);
-
-        printf("w: %i\n", w);//debug
-        c[i].push_back(w);
+        edges[i].push_back(u);
 
         printf("v: %i\n", v);//debug
-        c[i].push_back(v);
+        edges[i].push_back(v);
 
-        if(empty == true && c[i][1] != 0)
+        printf("w: %i\n", w);//debug
+        edges[i].push_back(w);
+        */
+        if(empty == true && edges[i][1] != 0)
             empty = false;
     }
+
+
+    /*makeset*/
+    for(size_t i = 0; i < tree_size; i++){
+        make_set(i);
+    }
+    
+    std::sort(edges.begin(), edges.end(),
+          [](const std::vector<int>& a, const std::vector<int>& b) {
+        return a[2] > b[2];
+    });
+
+    printf("\n +++ \n");
+
+    
+    for (auto i : edges){
+        printf("%i %i %i\n", i[0], i[1], i[2]);
+    }
+
 
     if(!empty){
         result = 1; //function to develop
@@ -48,3 +89,5 @@ int main() {
     std::cout << result << "\n";
     return 0;
 }
+
+ 
