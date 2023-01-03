c1b979ee421091fc65a8c8fc1f1bddb5163481c5
joaoafonso07 <joaoafonsomestre@gmail.com>
2023-01-03 13:31:50 +0000

    Nem vou falar disto

--- project.cpp
+++ project.cpp
@@ -1,75 +1,105 @@
-#include <stdio.h>
+//#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <climits>
 
 typedef struct {
     int parent;
     int rank;
 } node;
 
-static node *tree;
-size_t tree_size;
+typedef struct {
+    int u;
+    int v;
+    int w;
+} edge;
 
+//node *tree;
+//size_t tree_size;
 
-void make_set(size_t i){
+/*
+void make_set(std::vector <node> tree, size_t i){
     tree[i].rank = 0;
     tree[i].parent = i + 1;
 }
+*/
 
-int find_set(int x){
+int find_set(node tree[], int x){
     if (tree[x-1].parent != x)
-        tree[x-1].parent = find_set(tree[x-1].parent);
+        tree[x-1].parent = find_set(tree, tree[x-1].parent);
     return tree[x-1].parent;
 }
 
-void link(int x, int y){
+void link(node tree[], int x, int y){
+    //printf("link: %i %i\n", x, y); //debug
     if (tree[x-1].rank > tree[y-1].rank){
         tree[y-1].parent = x;
     }
     else{
+        //printf("hear\n"); //debug
         tree[x-1].parent = y;
-        if(tree[x-1].rank == tree[y-1].rank)
+        if(tree[x-1].rank == tree[y-1].rank){
+            //printf("rank==\n"); //debug
             tree[y-1].rank++;
+        }   
     }
 }
 
-void union_find(int x, int y){
-    link(find_set(x), find_set(y));
+void union_find(node tree[], int x, int y){
+    link(tree, find_set(tree, x), find_set(tree, y));
 }
 
 int main() {
     int E, u, w, v; //V = number of vertices, E = number of eges
+    unsigned long tree_size;
+    node *tree;
     unsigned long long result = 0;
-    bool empty = true;
+    //bool empty = true;
 
+    std::ios::sync_with_stdio(false);
+    /*
     if(scanf("%lu", &tree_size) == -1)
         return -1;
+    */
+    std::cin >> tree_size;
 
+    //std::vector<node> &tree;
+    
     tree = (node*)malloc(sizeof(node)*tree_size);
     if(tree == NULL){
         return -1;
     }
+    
 
+    /*
     if(scanf("%i", &E) == -1)
         return -1;
+    */
+    std::cin >> E;
 
-    std::vector<std::vector<int>> edges;  //vector with e number of vectors representing a heavy edge (u, v, w) 
+    std::vector<edge> edges(E);  //vector with e number of vectors representing a heavy edge (u, v, w) 
     
 
     /*
     Reads e number of heavy eges (u, v, w) being u and v two vertices conected with a w weight
     */
     for(short i = 0; i < E; i++){ // for each i less than E add the vector to the vector c
+        /*
         if(scanf("%i %i %i", &u, &v, &w) != 3){
             printf("something wrong with scanf\n");//debug
             return -1;
         }
-        edges.push_back({u, v, w});
-        
+        */
+        std::cin >> u >> v >> w;
+        edges[i].u = u;
+        edges[i].v = v;
+        edges[i].w = w;
+        /*
         if(empty == true && edges[i][1] != 0)
             empty = false;
+        */    
     }
 
 
@@ -77,12 +107,15 @@ int main() {
 
     /*makeset*/
     for(size_t i = 0; i < tree_size; i++){
-        make_set(i);
+        tree[i].rank = 0;
+        tree[i].parent = i + 1;
+        //make_set(tree, i);
     }
     
-    std::sort(edges.begin(), edges.end(),
-          [](const std::vector<int>& a, const std::vector<int>& b) {
-        return a[2] > b[2];
+    std::stable_sort(edges.begin(), edges.end(),
+          [](const edge &a, const edge &b) {
+        return a.w > b.w;
+    
     });
 
     //printf("\n +++ \n"); //debug
@@ -90,19 +123,22 @@ int main() {
     
     for (auto i : edges){
         //printf("%i %i %i\n", i[0], i[1], i[2]); //debug
-        if(find_set(i[0]) != find_set(i[1])){ //i thonk that is waht is causing trouble
-            union_find(i[0], i[1]);
-            result += i[2];
+        //printf("p: %i %i\n", tree[i[0]-1].parent, tree[i[1]-1].parent); //debug
+        if(find_set(tree, i.u) != find_set(tree, i.v)){ 
+            //printf("yes\n"); //debug
+            union_find(tree, i.u, i.v);
+            result += i.w;
         }
+        //printf("-----\n"); //debug
     }
 
-    /*
-    if(!empty){
-        result = 1; //function to develop
-    }else
-        result = 0;
-    */
+    
+
+
+    
     std::cout << result << "\n";
+
+    free(tree);
     return 0;
 }
 
